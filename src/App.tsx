6c0fc68d826ebe0159ea65bd75d9cfecfb3eb587
kingheedo <52102550+kingheedo@users.noreply.github.com>
2023-05-06 20:46:58 +0900

    fix(코드 수정)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import './App.scss';
-import Example1 from './components/Example1';
-import Example2 from './components/Example2';
 import Example3 from './components/Example3';
 
 function App() {
   return (
     <div className="App">
-      {/* <Example1/> */}
-      {/* <Example2/> */}
       <Example3/>
     </div>
   );
